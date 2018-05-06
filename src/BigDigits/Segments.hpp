791a4d19d228f6cdc20d5a8d5935cfbac4d49a5d
Elian Doran <elinnum@gmail.com>
2018-05-06 03:08:05 +0300

    Use constants instead of hard-coded values

--- src/BigDigits/Segments.hpp
+++ src/BigDigits/Segments.hpp
@@ -1,20 +1,21 @@
 #include <Arduino.h>
 
 #define NUM_DIGITS 10
+#define NUM_SEGMENTS 11
 #define SEGMENTS_PER_DIGIT 4
 
 enum BigDigitSegment {
-    SEGMENT_BLANK,
-    SEGMENT_LEFT,
-    SEGMENT_RIGHT,
-    SEGMENT_TOP,
-    SEGMENT_TOP_LEFT,
-    SEGMENT_TOP_RIGHT,
-    SEGMENT_TOP_BOTTOM,
-    SEGMENT_TOP_LEFT_BOTTOM,
-    SEGMENT_TOP_RIGHT_BOTTOM,
-    SEGMENT_BOTTOM_LEFT,
-    SEGMENT_BOTTOM_RIGHT
+    SEGMENT_BLANK = 0,
+    SEGMENT_LEFT = 1,
+    SEGMENT_RIGHT = 2,
+    SEGMENT_TOP = 3,
+    SEGMENT_TOP_LEFT = 4,
+    SEGMENT_TOP_RIGHT = 5,
+    SEGMENT_TOP_BOTTOM = 6,
+    SEGMENT_TOP_LEFT_BOTTOM = 7,
+    SEGMENT_TOP_RIGHT_BOTTOM = 8,
+    SEGMENT_BOTTOM_LEFT = 9,
+    SEGMENT_BOTTOM_RIGHT = 10
 };
 
 class BigDigit
@@ -64,15 +65,15 @@ private:
 
 public:
     void segmentDebug() {
-        int minSegments = 10;
+        int minSegments = NUM_SEGMENTS;
         int maxSegments = 0;
         int count = 0;
 
-        for (int digit0 = 0; digit0 <= 9; digit0++)
-        for (int digit1 = 0; digit1 <= 9; digit1++)
-        for (int digit2 = 0; digit2 <= 9; digit2++)
-        for (int digit3 = 0; digit3 <= 9; digit3++) {
-            bool segmentMap[10] = {};
+        for (int digit0 = 0; digit0 < NUM_DIGITS; digit0++)
+        for (int digit1 = 0; digit1 < NUM_DIGITS; digit1++)
+        for (int digit2 = 0; digit2 < NUM_DIGITS; digit2++)
+        for (int digit3 = 0; digit3 < NUM_DIGITS; digit3++) {
+            bool segmentMap[NUM_SEGMENTS] = {};
 
             insertSegmentsIntoSet(digit0, segmentMap);
             insertSegmentsIntoSet(digit1, segmentMap);
@@ -80,7 +81,7 @@ public:
             insertSegmentsIntoSet(digit3, segmentMap);
 
             int segmentCount = 0;
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < NUM_SEGMENTS; i++) {
                 if (segmentMap[i])
                     segmentCount++;
             }
