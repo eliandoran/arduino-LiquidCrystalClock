3f48f0d5cdf6d12ab343ce3cfbda4be533991f5a
Elian Doran <elinnum@gmail.com>
2018-05-06 03:46:38 +0300

    Try segments with a smaller top

--- src/BigDigits/Segments.hpp
+++ src/BigDigits/Segments.hpp
@@ -6,16 +6,16 @@
 
 enum BigDigitSegment {
     SEGMENT_BLANK = 0,
-    SEGMENT_LEFT = 1,
-    SEGMENT_RIGHT = 2,
-    SEGMENT_TOP = 3,
+    SEGMENT_RIGHT = 1,
+    SEGMENT_TOP = 2,
+    SEGMENT_TOP_BOTTOM = 3,
     SEGMENT_TOP_LEFT = 4,
-    SEGMENT_TOP_RIGHT = 5,
-    SEGMENT_TOP_BOTTOM = 6,
-    SEGMENT_TOP_LEFT_BOTTOM = 7,
-    SEGMENT_TOP_RIGHT_BOTTOM = 8,
+    SEGMENT_TOP_LEFT_BOTTOM = 5,
+    SEGMENT_TOP_RIGHT = 6,
+    SEGMENT_TOP_RIGHT_BOTTOM = 7,
+    SEGMENT_BOTTOM = 8,
     SEGMENT_BOTTOM_LEFT = 9,
-    SEGMENT_BOTTOM_RIGHT = 10
+    SEGMENT_BOTTOM_RIGHT = 10,
 };
 
 class BigDigit
@@ -31,36 +31,36 @@ private:
           SEGMENT_BLANK, SEGMENT_RIGHT },
 
         // Digit 2
-        { SEGMENT_TOP, SEGMENT_TOP_RIGHT,
-          SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_BOTTOM },
+        { SEGMENT_TOP_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM,
+          SEGMENT_BOTTOM_LEFT, SEGMENT_BOTTOM },
 
         // Digit 3
-        { SEGMENT_TOP, SEGMENT_TOP_RIGHT,
-          SEGMENT_TOP_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM },
+        { SEGMENT_TOP_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM,
+          SEGMENT_BOTTOM, SEGMENT_BOTTOM_RIGHT },
 
         // Digit 4
-        { SEGMENT_LEFT, SEGMENT_RIGHT,
-          SEGMENT_TOP, SEGMENT_TOP_RIGHT },
+        { SEGMENT_BOTTOM_LEFT, SEGMENT_BOTTOM_RIGHT,
+          SEGMENT_BLANK, SEGMENT_RIGHT },
 
         // Digit 5
-        { SEGMENT_TOP_LEFT, SEGMENT_TOP,
-          SEGMENT_TOP_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM },
+        { SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_BOTTOM,
+          SEGMENT_BOTTOM, SEGMENT_BOTTOM_RIGHT },
 
         // Digit 6
-        { SEGMENT_TOP_LEFT, SEGMENT_TOP,
-          SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM },
+        { SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_BOTTOM,
+          SEGMENT_BOTTOM_LEFT, SEGMENT_BOTTOM_RIGHT },
 
         // Digit 7
         { SEGMENT_TOP, SEGMENT_TOP_RIGHT,
           SEGMENT_BLANK, SEGMENT_RIGHT },
 
         // Digit 8
-        { SEGMENT_TOP_LEFT, SEGMENT_TOP_RIGHT,
-          SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM },
+        { SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM,
+          SEGMENT_BOTTOM_LEFT, SEGMENT_BOTTOM_RIGHT },
 
         // Digit 9
-        { SEGMENT_TOP_LEFT, SEGMENT_TOP_RIGHT,
-          SEGMENT_TOP, SEGMENT_TOP_RIGHT }
+        { SEGMENT_TOP_LEFT_BOTTOM, SEGMENT_TOP_RIGHT_BOTTOM,
+          SEGMENT_BLANK, SEGMENT_RIGHT }
     };
 
 public:
@@ -69,13 +69,15 @@ public:
         int maxSegments = 0;
         int count = 0;
 
-        for (int digit0 = 0; digit0 < NUM_DIGITS; digit0++)
-        for (int digit1 = 0; digit1 < NUM_DIGITS; digit1++)
-        for (int digit2 = 0; digit2 < NUM_DIGITS; digit2++)
-        for (int digit3 = 0; digit3 < NUM_DIGITS; digit3++) {
+        for (int digit0 = 0; digit0 < 2; digit0++)
+        for (int digit1 = 0; digit1 < (digit0 == 1 ? 2 : 9); digit1++)
+        for (int digit2 = 0; digit2 < 5; digit2++)
+        for (int digit3 = 0; digit3 < 10; digit3++) {
             bool segmentMap[NUM_SEGMENTS] = {};
 
-            insertSegmentsIntoSet(digit0, segmentMap);
+            if (digit0 != 0)
+                insertSegmentsIntoSet(digit0, segmentMap);
+                
             insertSegmentsIntoSet(digit1, segmentMap);
             insertSegmentsIntoSet(digit2, segmentMap);
             insertSegmentsIntoSet(digit3, segmentMap);
@@ -84,6 +86,13 @@ public:
             for (int i = 0; i < NUM_SEGMENTS; i++) {
                 if (segmentMap[i])
                     segmentCount++;
+            }
+
+            if (segmentCount == 9) {
+                Serial.print(digit0);
+                Serial.print(digit1);
+                Serial.print(digit2);
+                Serial.println(digit3);
             }
 
             minSegments = min(minSegments, segmentCount);
@@ -98,7 +107,10 @@ public:
 
     void insertSegmentsIntoSet(int digit, bool segmentMap[]) {
         for (int i=0; i<SEGMENTS_PER_DIGIT; i++) {
-            segmentMap[this->digits[digit][i]] = true;
+            int segment = this->digits[digit][i];
+
+            if (segment != SEGMENT_BLANK)
+                segmentMap[segment] = true;
         }
     }
 };
